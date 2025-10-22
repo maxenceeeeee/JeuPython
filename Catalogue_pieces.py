a567908111f6df7b6f2097f2ca472af2d1da727b
aIennnn <alen.grebenc@etu.sorbonne-universite.fr>
2025-10-22 14:07:14 +0200

    Ajout du coût et de la rareté
    ajoute cout_gemmes et rarete à chaque pièce

--- Catalogue_pieces.py
+++ Catalogue_pieces.py
@@ -1,6 +1,6 @@
-
 """
 Dictionnaire de 15 pièces représentatives du jeu, avec nom, couleur, portes et items.
+
 """
 
 catalogue_pieces = [
@@ -9,105 +9,120 @@ catalogue_pieces = [
         "couleur": "bleue",
         "portes": {"haut": True, "bas": False, "gauche": True, "droite": True},
         "items": ["lettre", "plan", "allowance token"],
-        "image" : "Images/entrancehall.webp"
+        "image" : "Images/entrancehall.webp",
+        "cout_gemmes": 0, "rarete": 0 
     },
     {
         "nom": "The Foundation",
         "couleur": "grise",
         "portes": {"haut": True, "bas": True, "gauche": False, "droite": False},
         "items": ["pierre ancienne", "blueprint part"],
-        "image" : "Images/foundation.webp"
+        "image" : "Images/foundation.webp",
+        "cout_gemmes": 0, "rarete": 1
     },
     {
         "nom": "Nook",
         "couleur": "bleue",
         "portes": {"haut": False, "bas": False, "gauche": True, "droite": True},
         "items": ["clé", "note noire", "livre"],
-        "image" : "Images/nook.webp"
+        "image" : "Images/nook.webp",
+        "cout_gemmes": 0, "rarete": 0
     },
     {
         "nom": "Garage",
         "couleur": "bleue",
         "portes": {"haut": False, "bas": True, "gauche": True, "droite": False},
         "items": ["voiture", "clé", "coffre dans le coffre"],
-        "image" : "Images/garage.webp"
+        "image" : "Images/garage.webp",
+        "cout_gemmes": 1, "rarete": 1
     },
     {
         "nom": "Music Room",
         "couleur": "bleue",
         "portes": {"haut": False, "bas": False, "gauche": True, "droite": True},
         "items": ["partitions", "clé spéciale"],
-        "image" : "Images/musicroom.webp"
+        "image" : "Images/musicroom.webp",
+        "cout_gemmes": 1, "rarete": 1
     },
     {
         "nom": "Locker Room",
         "couleur": "bleue",
         "portes": {"haut": False, "bas": True, "gauche": True, "droite": False},
         "items": ["casiers", "clé", "pièces"],
-        "image" : "Images/lockerroom.webp"
+        "image" : "Images/lockerroom.webp",
+        "cout_gemmes": 0, "rarete": 0
     },
     {
         "nom": "Den",
         "couleur": "bleue",
         "portes": {"haut": True, "bas": True, "gauche": False, "droite": False},
         "items": ["gemme", "coffre verrouillé"],
-        "image" : "Images/den.webp"
+        "image" : "Images/den.webp",
+        "cout_gemmes": 2, "rarete": 2 # Contient une gemme, donc plus rare/chère
     },
     {
         "nom": "Wine Cellar",
         "couleur": "bleue",
         "portes": {"haut": False, "bas": True, "gauche": False, "droite": True},
         "items": ["bouteilles", "gemmes"],
-        "image" : "Images/winecellar.webp"
+        "image" : "Images/winecellar.webp",
+        "cout_gemmes": 1, "rarete": 1
     },
     {
         "nom": "Trophy Room",
-        "couleur": "dorée",
+        "couleur": "dorée", # Jaune (Magasin)
         "portes": {"haut": False, "bas": False, "gauche": True, "droite": True},
         "items": ["trophées", "gemmes"],
-        "image" : "Images/trophyroom.webp"
+        "image" : "Images/trophyroom.webp",
+        "cout_gemmes": 2, "rarete": 2
     },
     {
         "nom": "Ballroom",
         "couleur": "blanche",
         "portes": {"haut": True, "bas": False, "gauche": False, "droite": True},
         "items": ["lustre", "partition", "clé de scène"],
-        "image" : "Images/ballroom.webp"
+        "image" : "Images/ballroom.webp",
+        "cout_gemmes": 1, "rarete": 1
     },
     {
         "nom": "Pantry",
-        "couleur": "verte",
+        "couleur": "verte", # Verte 
         "portes": {"haut": False, "bas": False, "gauche": True, "droite": True},
         "items": ["fruit", "pièces"],
-        "image" : "Images/pantry.webp"
+        "image" : "Images/pantry.webp",
+        "cout_gemmes": 0, "rarete": 0 # Contient de la nourriture 
     },
     {
         "nom": "Rumpus Room",
         "couleur": "bleue",
         "portes": {"haut": True, "bas": False, "gauche": True, "droite": False},
         "items": ["machine Alzara", "pièces"],
-        "image" : "Images/rumpusroom.webp"
+        "image" : "Images/rumpusroom.webp",
+        "cout_gemmes": 1, "rarete": 1
     },
     {
         "nom": "Observatory",
-        "couleur": "violette",
+        "couleur": "violette", # Violette (Chambre)
         "portes": {"haut": True, "bas": False, "gauche": True, "droite": True},
         "items": ["télescope", "constellation map"],
-        "image" : "Images/observatory.webp"
+        "image" : "Images/observatory.webp",
+        "cout_gemmes": 2, "rarete": 2
     },
     {
         "nom": "Library",
         "couleur": "brune",
         "portes": {"haut": True, "bas": False, "gauche": True, "droite": True},
         "items": ["livres", "note de recherche"],
-        "image" : "Images/library.webp"
+        "image" : "Images/library.webp",
+        "cout_gemmes": 1, "rarete": 1
     },
     {
-        "nom": "Vault",
-        "couleur": "dorée",
+        "nom": "Vault", # Exemple du PDF
+        "couleur": "dorée", # Jaune (Magasin))
         "portes": {"haut": False, "bas": False, "gauche": True, "droite": False},
         "items": ["coffre-fort", "gemmes", "clés"],
-        "image" : "Images/vault.webp"
+        "image" : "Images/vault.webp",
+        "cout_gemmes": 3, "rarete": 3 
     },
     
     {
@@ -115,6 +130,7 @@ catalogue_pieces = [
         "couleur": "bleue",
         "portes": {"haut": False, "bas": False, "gauche": True, "droite": False},
         "items": [],
-        "image" : "Images/antechamber.webp"
+        "image" : "Images/antechamber.webp",
+        "cout_gemmes": 0, "rarete": 0
     }
 ]
